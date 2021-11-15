8c5bb2e71828e4bba9b86b6d620020738a81beef
mina-beiki <mina_beiki@yahoo.com>
2021-11-15 14:00:39 +0330

    Added initial code

---
+++ client.c
@@ -0,0 +1,83 @@
+/*
+Client is a simple user interface to get the data
+and send it to the server by socket.
+Every client can do the following commands:
+    1. Start
+    2. Ping 
+    3. Stop
+*/
+// socket libraries
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+//
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    // TODO: How does socket create?
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: What is memory cell?
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    // TODO: What is address family
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    // TODO: Why do we convert IPv4 and IPv6 to binary
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connecting to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // User commands
+    int valread;
+    char buffer[1024] = {0};
+    char command[50];
+
+    while (1)
+    {
+        printf("> ");
+        gets(command);
+
+        send(sock, command, strlen(command), 0);
+
+        if (strcmp(command, "stop") == 0)
+        {
+            printf("Disconnected\n");
+            break;
+        }
+
+        valread = read(sock, buffer, sizeof(buffer));
+
+        if (valread < 0)
+        {
+            perror("Reading failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("buffer= %s\n", buffer);
+    }
+    
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,105 @@
+/* 
+Using sockets to connect to our server.
+Our server which will accept clients and give response to them.
+Our main input command are: 
+    1. Start [name]
+    2. Ping 
+    3. Stop
+Server responses:
+    1. Init user
+    2. Pong [status]
+    3. Release client
+*/
+
+// Socket libraries
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+//
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+// Thread library
+#include <pthread.h>
+
+int main(int argc, char const *argv[])
+{
+    // TODO: What is a socket file descriptor
+    int server_fd;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0); // TODO: What is this do
+    if (server_fd == 0)
+    {
+        perror("Socket faild");
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: What the fuck are these??
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(atoi(argv[1]));
+    const int addrlen = sizeof(address);
+
+    // TODO: What is binding??
+    if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0)
+    {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: What is backlog?
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("Listen faild");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Listening on %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+    // Accepting client
+    
+    int client_socket;
+    if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("Accept faild");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Accepted client %s:%d id:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port), client_socket);
+
+
+    int valread;
+    char buffer[1024] = {0};
+    char response[1024] = {0};
+
+
+    while (1)
+    {
+        valread = read(client_socket, buffer, sizeof(buffer));
+        if (valread < 0)
+        {
+            perror("Empty read");
+            exit(EXIT_FAILURE);
+        }
+
+        buffer[valread] = '\0';
+        send(client_socket, buffer, sizeof(buffer), 0);
+
+        if (strcmp(buffer, "stop") == 0)
+        {
+            printf("Client %d: disconnected\n", client_socket);
+            break;
+        }
+
+        printf("Message: %s\n", buffer);
+
+        fflush(stdout);
+        buffer[0] = '\0';
+    }
+    
+
+    return 0;
+}
